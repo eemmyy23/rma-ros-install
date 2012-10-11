df5b970a102a17b505fa16574821345fa490c8bf
emi.puscalau@gmail.com <emi.puscalau@gmail.com@08f92d26-aeb0-39de-79c0-1f6dd505bb69>
2012-10-11 18:05:26 +0000

    workspace

--- trunk/ros-electric-install-ub1010.sh
+++ trunk/ros-electric-install-ub1010.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu maverick main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install ros-electric-ros-base
+sudo apt-get -y install ros-electric-ros-base
 echo "source /opt/ros/electric/setup.bash" >> $HOME/.bashrc
 source /opt/ros/electric/setup.bash
---
+++ trunk/ros-workspace.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+sudo apt-get -y install python-pip
+sudo pip install -U rosinstall --proxy=$http_proxy
+sudo pip install -U rosinstall_shellcompletion --proxy=$http_proxy
+mkdir $HOME/work_ros_electric
+rosws init $HOME/ros_electric /opt/ros/electric
+echo "source $HOME/ros_electric/setup.bash" >> $HOME/.bashrc
