ae06c155c94c1767c37b33845210c990c5a9acb2
emi.puscalau@gmail.com <emi.puscalau@gmail.com@08f92d26-aeb0-39de-79c0-1f6dd505bb69>
2012-10-17 13:28:07 +0000

    sepparate pkg script

--- trunk/ros-workspace.sh
+++ trunk/ros-workspace.sh
@@ -2,12 +2,10 @@
 rosDistributinoName="electric"
 rosWorkspaceDir="$HOME/ros_$rosDistributinoName"
 
-sudo apt-get -y install ros-electric-geometry
-sudo apt-get -y install ros-electric-common
-
 sudo apt-get -y install python-pip
 sudo pip install -U rosinstall --proxy=$http_proxy
 sudo pip install -U rosinstall_shellcompletion --proxy=$http_proxy
+
 
 if [ -d "$rosWorkspaceDir" ]; then 
 echo "do you want to override"
@@ -23,5 +21,11 @@ mkdir $rosWorkspaceDir
 rosws init $rosWorkspaceDir /opt/ros/$rosDistributinoName
 echo "source ${rosWorkspaceDir}/setup.bash" >> $HOME/.bashrc
 source ${rosWorkspaceDir}/setup.bash
+
+
+sudo apt-get -y install ros-electric-geometry
+sudo apt-get -y install ros-electric-common
+
+
 
 
