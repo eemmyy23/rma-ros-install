ed24d4860f91aba91e856d5b14f66625915c979c
emi.puscalau@gmail.com <emi.puscalau@gmail.com@08f92d26-aeb0-39de-79c0-1f6dd505bb69>
2012-10-18 13:00:33 +0000

    new pmd

---
+++ packages/pmd_camcube_3_ros_pkg/build/CMakeFiles/ROSBUILD_genmsg_py.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "../src/pmd_camcube_3_ros_pkg/msg"
+  "../msg_gen"
+  "../msg_gen"
+  "CMakeFiles/ROSBUILD_genmsg_py"
+  "../src/pmd_camcube_3_ros_pkg/msg/__init__.py"
+  "../src/pmd_camcube_3_ros_pkg/msg/_Distance.py"
+  "../src/pmd_camcube_3_ros_pkg/msg/_Pixels.py"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ROSBUILD_genmsg_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
