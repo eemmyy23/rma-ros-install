9e8a38df252650430a48f24d339d264c875304b6
emi.puscalau@gmail.com <emi.puscalau@gmail.com@08f92d26-aeb0-39de-79c0-1f6dd505bb69>
2012-11-13 13:57:59 +0000

    q

--- trunk/pioneer/install.sh
+++ trunk/pioneer/install.sh
@@ -1,3 +1,14 @@
 #for pioneer robot
 #using ubuntu 12.04 Server
 #install ros-fuerte
+#export http_proxy="http://proxy2.intra.rma.ac.be:3128"
+#export https_proxy="http://proxy2.intra.rma.ac.be:3128"
+#export ftp_proxy="http://proxy2.intra.rma.ac.be:3128"
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
+
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+
+sudo apt-get update
+
+sudo apt-get install -y ros-fuerte-ros-comm
