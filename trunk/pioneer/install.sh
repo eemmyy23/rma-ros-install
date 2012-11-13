0341dd80ab88e7846911f697a38a27fd689ec0a1
emi.puscalau@gmail.com <emi.puscalau@gmail.com@08f92d26-aeb0-39de-79c0-1f6dd505bb69>
2012-11-13 14:06:52 +0000

    q

--- trunk/pioneer/install.sh
+++ trunk/pioneer/install.sh
@@ -12,3 +12,23 @@ wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 
 sudo apt-get install -y ros-fuerte-ros-comm
+
+sudo apt-get -y install subversion
+#is there a proxy? svn may not be configured
+if [ -n "$http_proxy" ]; then
+    echo "proxy is set: $http_proxy"
+                tmp=`echo -n ${http_proxy} | cut -d/ -f3`
+                host=`echo $tmp | cut -d: -f1`
+                port=`echo $tmp | cut -d: -f2`    
+                svnConfigFile="$HOME/.subversion/servers"
+    if [ `cat $svnConfigFile | grep -c $host` -eq 0 ];then
+        echo "proxy is not set for svn"
+        echo "http-proxy-host = $host" >> $svnConfigFile
+        echo "http-proxy-port = $port" >> $svnConfigFile
+        echo "$svnConfigFile was configured with your proxy settings"
+    else
+        echo "proxy settings are already applied for svn"
+    fi
+else
+    echo '$http_proxy is not set'
+fi
